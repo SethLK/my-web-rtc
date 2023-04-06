5846212cd9020b93431f2ee6ffb84365f959b539
SethLK <sethlk2006@gmail.com>
2023-04-06 21:43:11 +0630

    Raw

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+const port = process.env.PORT || 3000;
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+app.use('/peerjs', peerServer);
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', (socket) => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId);
+        });
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
