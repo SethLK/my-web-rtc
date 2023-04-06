76925dd958acd960a3042bd6e6daaf9cef4cbdb1
SethLK <sethlk2006@gmail.com>
2023-04-06 21:50:17 +0630

    raw 2

--- server.js
+++ server.js
@@ -1,38 +1,71 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
+
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
-    debug: true,
+  debug: true
 });
-const port = process.env.PORT || 3000;
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 
-app.use('/peerjs', peerServer);
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
+app.use('/peerjs', peerServer); 
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var name = ''; // defining "name" in the global scope
+let id = Math.round(Math.random() * 100000000 + 1);
 
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidv4()}`);
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room });
+app.get('/host', (req, res) => {
+  name = req.query.name;
+  res.redirect(`/room/${id}`);
 });
 
-io.on('connection', (socket) => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected', userId);
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId);
-        });
-    });
+app.get('/join', (req, res) => {
+  name = req.query.name;
+  res.redirect(`/room/${req.query.room}`);
 });
 
-server.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+// Define the delay time in milliseconds
+// const redirectDelay = 3000;
+
+// app.get('/room/:room', (req, res) => {
+//   const roomId = req.params.room;
+//   if (roomId === id.toString()) {
+//     res.render('room', { roomId, Myname: name });
+//   } else {
+//     setTimeout(() => {
+//       res.redirect('/');
+//     }, redirectDelay);
+//   }
+// });
+
+app.get('/room/:room', (req, res) => {
+  const roomId = req.params.room;
+  if (roomId === id.toString()) {
+    res.render('room', { roomId, Myname: name });
+  } else {
+    res.redirect('/');
+  }
+});
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.broadcast.to(roomId).emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.broadcast.to(roomId).emit('user-disconnected', userId)
+    })
+  })
+})
+server.listen(3000, () => {
+    console.log(`Server started on port 3000`);
 });
 
