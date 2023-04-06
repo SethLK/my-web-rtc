ee0a35429e76840e68b06717fd897702fcb480e5
SethLK <sethlk2006@gmail.com>
2023-04-06 22:04:55 +0630

    Good internet app didn't crash

---
+++ public/src/script.js
@@ -0,0 +1,67 @@
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: 3000,
+  path: '/peerjs'
+});
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+const peers = {};
+
+myPeer.on('open', id => {
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  }).then(stream => {
+    addVideoStream(myVideo, stream);
+
+    socket.emit('join-room', ROOM_ID, id);
+
+    myPeer.on('call', call => {
+      call.answer(stream);
+      const video = document.createElement('video');
+      call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+      });
+      call.on('close', () => {
+        video.remove();
+      });
+
+      peers[call.peer] = call;
+    });
+
+    socket.on('user-connected', userId => {
+      connectToNewUser(userId, stream);
+    });
+
+    socket.on('user-disconnected', userId => {
+      if (peers[userId]) peers[userId].close();
+    });
+  }).catch(error => {
+    console.log(error);
+  });
+});
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on('close', () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
