3e3542b92e9a34d810cc3d98611707041ace4483
SethLK <sethlk2006@gmail.com>
2023-04-18 21:40:46 +0630

    Mic and Video are in one div or container, and work well

--- public/src/script.js
+++ public/src/script.js
@@ -58,10 +58,68 @@ function connectToNewUser(userId, stream) {
   peers[userId] = call;
 }
 
+// function addVideoStream(video, stream) {
+//   video.srcObject = stream;
+//   video.addEventListener('loadedmetadata', () => {
+//     video.play();
+//   });
+//   mic_and_video()
+//   videoGrid.append(video);
+// }
+
+// function mic_and_video() {
+//   const controls = document.createElement('div');
+//   controls.classList.add('controls');
+  
+//   const muteButton = document.createElement('button');
+//   muteButton.innerText = 'Mute';
+//   muteButton.addEventListener('click', () => {
+//     stream.getAudioTracks()[0].enabled = !stream.getAudioTracks()[0].enabled;
+//     muteButton.innerText = stream.getAudioTracks()[0].enabled ? 'Mute' : 'Unmute';
+//   });
+//   controls.appendChild(muteButton);
+
+//   const videoButton = document.createElement('button');
+//   videoButton.innerText = 'Stop Video';
+//   videoButton.addEventListener('click', () => {
+//     stream.getVideoTracks()[0].enabled = !stream.getVideoTracks()[0].enabled;
+//     videoButton.innerText = stream.getVideoTracks()[0].enabled ? 'Stop Video' : 'Start Video';
+//   });
+//   controls.appendChild(videoButton);
+
+//   videoGrid.appendChild(controls);
+// }
+
+
 function addVideoStream(video, stream) {
+  const videoContainer = document.createElement('div');
+  videoContainer.classList.add('video-container');
+  
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
     video.play();
   });
-  videoGrid.append(video);
+  videoContainer.appendChild(video);
+
+  const controls = document.createElement('div');
+  controls.classList.add('controls');
+  
+  const muteButton = document.createElement('button');
+  muteButton.innerText = 'Mute';
+  muteButton.addEventListener('click', () => {
+    stream.getAudioTracks()[0].enabled = !stream.getAudioTracks()[0].enabled;
+    muteButton.innerText = stream.getAudioTracks()[0].enabled ? 'Mute' : 'Unmute';
+  });
+  controls.appendChild(muteButton);
+
+  const videoButton = document.createElement('button');
+  videoButton.innerText = 'Stop Video';
+  videoButton.addEventListener('click', () => {
+    stream.getVideoTracks()[0].enabled = !stream.getVideoTracks()[0].enabled;
+    videoButton.innerText = stream.getVideoTracks()[0].enabled ? 'Stop Video' : 'Start Video';
+  });
+  controls.appendChild(videoButton);
+
+  videoContainer.appendChild(controls);
+  videoGrid.appendChild(videoContainer);
 }
