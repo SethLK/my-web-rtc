0fdaea4a48071e9a3a9566007a8af523616b15b0
SethLK <sethlk2006@gmail.com>
2023-05-25 21:41:17 +0630

    စိတ်မချလို့ပါ

--- public/src/script.js
+++ public/src/script.js
@@ -107,10 +107,10 @@ mic_m.addEventListener('click', () => {
 </svg> Unmute`;
 });
 
-const linkBtn = document.createElement('button');
-linkBtn.type = 'button';
-linkBtn.classList.add('btn', 'btn-danger');
-linkBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right align" viewBox="0 0 16 16">
+const leaveBtn = document.createElement('button');
+leaveBtn.type = 'button';
+leaveBtn.classList.add('btn', 'btn-danger');
+leaveBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right align" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
 <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
 </svg> Leave`
@@ -119,13 +119,11 @@ linkBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="
 const linkUrl = '/';
 
 // Add a click event listener to redirect to the link URL
-linkBtn.addEventListener('click', () => {
+leaveBtn.addEventListener('click', () => {
   window.location.href = linkUrl;
 });
 
 // Append the link button to a container element
 
-
-
-wrapper.append(video_m, mic_m, linkBtn);
+wrapper.append(video_m, mic_m, leaveBtn, );
 
